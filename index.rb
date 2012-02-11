a3152a5d3e7666543ff523ba535b307a2b4fc77d
Steven Klise <sklise@gmail.com>
2012-02-11 18:28:51 -0500

    Added authentication

--- index.rb
+++ index.rb
@@ -8,6 +8,12 @@ configure do |c|
   set :scess, :style => :compact
   set :database, ENV['DATABASE_URL'] || "sqlite3://db/development.sqlite"
   ActiveRecord::Base.include_root_in_json = false
+
+  use Rack::Auth::Basic, "Restricted Area" do |username, password|
+    u = ENV['USERNAME'] || 'admin'
+    p = ENV['PASSWORD'] || 'admin'
+    [username, password] == [u, p]
+  end
 end
 
 require './app/models'
