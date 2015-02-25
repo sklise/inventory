7f58ca970ad754cc5b5ca491087d61fda8d86c4e
Steve Klise <sk@skli.se>
2015-02-24 23:33:31 -0500

    Authors create

--- inventory.go
+++ inventory.go
@@ -65,13 +65,13 @@ func main() {
   }).Methods("Get")
 
   ro.HandleFunc("/things", func(w http.ResponseWriter, r *http.Request) {
-    authors := []Thing{}
-    db.Find(&authors)
-    fmt.Printf("%v", len(authors))
-    for i := 0; i < len(authors); i++ {
-      fmt.Printf("%v", authors[i])
+    things := []Thing{}
+    db.Find(&things)
+    fmt.Printf("%v", len(things))
+    for i := 0; i < len(things); i++ {
+      fmt.Printf("%v", things[i])
     }
-    re.HTML(w, 200, "things/index", authors)
+    re.HTML(w, 200, "things/index", things)
   }).Methods("Get")
 
   ro.HandleFunc("/things/new", func(w http.ResponseWriter, r *http.Request) {
@@ -100,7 +100,7 @@ func main() {
     db.Where("id = ?", vars["id"]).First(&thing)
 
     if thing.Id == 0 {
-      re.HTML(w, 404, "404", "")
+      re.HTML(w, 404, "error", "404 Could not find requested thing")
     } else {
       re.HTML(w, 200, "things/show", thing)
     }
@@ -115,6 +115,48 @@ func main() {
     vars := context.Get(r, "params")
     fmt.Fprintf(w, "ID: %v", vars)
   }).Methods("Delete")
+
+  // Index for Authors
+  ro.HandleFunc("/authors", func(w http.ResponseWriter, r *http.Request) {
+    authors := []Author{}
+    db.Find(&authors)
+
+    re.HTML(w, 200, "authors/index", authors)
+  }).Methods("Get")
+
+  // New Author Form
+  ro.HandleFunc("/authors/new", func(w http.ResponseWriter, r *http.Request) {
+    re.HTML(w, 200, "authors/new","")
+  }).Methods("Get")
+
+  // Create Author
+  ro.HandleFunc("/authors", func(w http.ResponseWriter, r *http.Request) {
+    err1 := r.ParseForm()
+    if err1 != nil {
+      re.HTML(w, 500, "error", 500)
+      return
+    }
+
+    author := Author {
+      Name: r.FormValue("name"),
+    }
+
+    db.Create(&author)
+
+    http.Redirect(w,r,"/authors",http.StatusFound)
+
+  }).Methods("Post")
+
+  // Show Author
+  ro.HandleFunc("/authors/{id}", func(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    author := Author{}
+    if db.Where("id = ?", vars["id"]).First(&author).RecordNotFound() {
+      re.HTML(w, 404, "error", "404 Could not find requested author")
+      return
+    }
+    re.HTML(w, 200, "authors/show", author)
+  }).Methods("Get")
 
   http.Handle("/", ro)
   http.ListenAndServe(":8080", nil)
